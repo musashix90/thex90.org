013535ec5d161547cd26fee45252cee5c2cfbee0
Michael Tanner <mtanner@mx90.net>
2017-10-05 07:07:26 -0400

    Updated to include the post form

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -1,7 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-  <h2 class="text-center">
-    Create a new post
-  </h2>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      Create a new post
+    </div>
+    <div class="panel-body">
+      <form action="/post/store" method="post">
+        {{ csrf_field() }}
+
+        <div class="form-group">
+          <label for="title">Title</label>
+          <input type="text" name="title" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="title">Featured image</label>
+          <input type="file" name="featured_img" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="content">Content</label>
+          <textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
+        </div>
+
+        <div class="form-group">
+          <div class="text-center">
+            <button class="btn btn-success" type="submit">
+              Store post
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 @endsection
