79f1ee6c7891cfc5fc3471c3f6a5af4ed6910ef9
Michael Tanner <mtanner@mx90.net>
2017-10-05 18:57:47 -0400

    Moved the homepage to the admin section

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('/home', 'HomeController@index')->name('home');
+
     Route::get('/post/create', [
         'uses' => 'PostController@create',
         'as' => 'post.create'
