65d3c3b0c064ebb0a4516fa9e44f266a0a4afdeb
Michael Tanner <mtanner@mx90.net>
2017-10-05 07:07:52 -0400

    Added the post/store route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,8 @@ Route::get('/post/create', [
 	'uses' => 'PostController@create',
 	'as' => 'post.create'
 ]);
+
+Route::post('/post/store', [
+    'uses' => 'PostController@store',
+    'as' => 'post.store'
+]);
