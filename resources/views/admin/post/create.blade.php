042cffef1c609ea46359efb9f6738d245075a74e
Michael Tanner <mtanner@mx90.net>
2017-10-05 19:45:53 -0400

    Fixed indention

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -2,47 +2,47 @@
 
 @section('content')
 
-  @if(count($errors) > 0)
-    <ul class="list-group">
-      @foreach($errors->all() as $error)
-        <li class="list-group-item text-danger">
-          {{ $error }}
-        </li>
-      @endforeach
-    </ul>
-  @endif
+    @if(count($errors) > 0)
+        <ul class="list-group">
+            @foreach($errors->all() as $error)
+                <li class="list-group-item text-danger">
+                    {{ $error }}
+                </li>
+            @endforeach
+        </ul>
+    @endif
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      Create a new post
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Create a new post
+        </div>
+        <div class="panel-body">
+            <form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
+                {{ csrf_field() }}
+
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="featured_img">Featured image</label>
+                    <input type="file" name="featured_img" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
+                </div>
+
+                <div class="form-group">
+                    <div class="text-center">
+                        <button class="btn btn-success" type="submit">
+                            Store post
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-    <div class="panel-body">
-      <form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
-        {{ csrf_field() }}
-
-        <div class="form-group">
-          <label for="title">Title</label>
-          <input type="text" name="title" class="form-control">
-        </div>
-
-        <div class="form-group">
-          <label for="featured_img">Featured image</label>
-          <input type="file" name="featured_img" class="form-control">
-        </div>
-
-        <div class="form-group">
-          <label for="content">Content</label>
-          <textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
-        </div>
-
-        <div class="form-group">
-          <div class="text-center">
-            <button class="btn btn-success" type="submit">
-              Store post
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
 @endsection
