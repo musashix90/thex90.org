a30ed7fa8764c5601428993ab7c3d50da8a9f8ef
Michael Tanner <mtanner@mx90.net>
2017-10-05 19:02:40 -0400

    Fixed an issue with the tabbing, and updated the links to use routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,23 +72,25 @@
         </nav>
 
         <div class="container">
-          <div class="row">
-            <div class="col-lg-4">
-              <ul class="list-group">
-                <li class="list-group-item">
-                  <a href="/home">Home</a>
-                </li>
-                <li class="list-group-item">
-                  <a href="/post/create">Create new post</a>
-                </li>
-             </ul>
-           </div>
-           <div class="col-lg-8">
-             @yield('content')
-           </div>
+            <div class="row">
+                @if(Auth::check())
+                    <div class="col-lg-4">
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <a href="{{ route('home') }}">Home</a>
+                            </li>
+                            <li class="list-group-item">
+                                <a href="{{ route('post.create') }}">Create new post</a>
+                            </li>
+                        </ul>
+                    </div>
+                @endif
+                <div class="col-lg-8">
+                    @yield('content')
+                </div>
+            </div>
         </div>
     </div>
-
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
