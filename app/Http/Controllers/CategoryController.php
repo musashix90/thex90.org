e7710dec5cd17f4a623356e953a5084065b5b165
Michael Tanner <mtanner@mx90.net>
2017-10-05 19:53:00 -0400

    Added validation on when creating a category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -34,7 +34,9 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
     }
 
     /**
